f541424478d7d4852cc5eef1daff12f34c83d24d
unknown <kiljh@me.com>
2020-05-16 11:37:40 +0900

    jquery 활용

--- color.js
+++ color.js
@@ -1,19 +1,22 @@
 var Links = {
   SetColor:function(color) {
-    let aList = document.querySelectorAll('a');
-
-    for (let index=0;index<aList.length;index++) {
-      aList[index].style.color=color;
-    }
+    // let aList = document.querySelectorAll('a');
+    //
+    // for (let index=0;index<aList.length;index++) {
+    //   aList[index].style.color=color;
+    // }
+    $('a').css("color",color)
   }
 }
 
 var Body = {
   SetColor:function (color){
-    document.querySelector('body').style.color=color;
+    // document.querySelector('body').style.color=color;
+    $('body').css('color', color);
   },
   SetBackgroundColor: function(color) {
-    document.querySelector('body').style.backgroundColor=color;
+    // document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor', color);
   }
 }
 function nightDayHandler(self) {
